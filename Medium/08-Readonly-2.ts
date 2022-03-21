9b3e2c18444482bb39e1e25d1669abe0feb04e77
lbw <574524020@qq.com>
2022-03-21 18:09:13 +0800

    Readonly2

---
+++ Medium/08-Readonly-2.ts
@@ -0,0 +1,21 @@
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+const todo: MyReadonly2<Todo, 'title' | 'description'> = {
+  title: "Hey",
+  description: "foobar",
+  completed: false,
+}
+
+todo.title = "Hello" // Error: cannot reassign a readonly property
+todo.description = "barFoo" // Error: cannot reassign a readonly property
+todo.completed = true // OK
+
+type MyReadonly2<T, U> = {
+  readonly [key in keyof T as key extends U ? key : never]: T[key];
+} & {
+  [K in keyof T]: T[K]
+}
