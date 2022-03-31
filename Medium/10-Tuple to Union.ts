4d35608edb197e41beda91ea6e90a195f19b33e0
lbw <574524020@qq.com>
2022-03-31 15:45:08 +0800

    fix

--- Medium/10-Tuple to Union.ts
+++ Medium/10-Tuple to Union.ts
@@ -3,6 +3,6 @@ type Arr = ['1', '2', '3']
 type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
 type Test2 = TupleToUnion2<Arr>
 
-export type TupleToUnion<T extends unknown[]> = T[number] 
+export type TupleToUnion<T extends readonly unknown[]> = T[number] 
 
-export type TupleToUnion2<T> = T extends [... infer A] ? A[number] : never;
+export type TupleToUnion2<T> = T extends readonly [... infer A] ? A[number] : never;
