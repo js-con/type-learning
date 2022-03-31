03a186920f43d1310993f7816ff30b395724f6d5
lbw <574524020@qq.com>
2022-03-31 12:04:19 +0800

    Pop

---
+++ Medium/16-Pop.ts
@@ -0,0 +1,10 @@
+//Implement a generic Pop<T> that takes an Array T and returns an Array without it's last element.
+
+// Example:
+type arr1 = ['a', 'b', 'c', 'd']
+type arr2 = [3, 2, 1]
+
+type re1 = Pop<arr1> // expected to be ['a', 'b', 'c']
+type re2 = Pop<arr2> // expected to be [3, 2]
+
+export type Pop<T extends unknown[]> = T['length'] extends 0 | 1 ? [] : T extends [...infer A, infer B] ? A : never
