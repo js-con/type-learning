5dcf34b8524163fc0ba34fdbe8a586702e20404b
lbw <574524020@qq.com>
2022-03-31 15:45:16 +0800

    Unshift

--- Easy/533-Concat&3057-Push&3060-Unshift.ts
+++ Easy/533-Concat&3057-Push&3060-Unshift.ts
@@ -6,3 +6,7 @@ export type Concat<A1 extends unknown[], A2 extends unknown[]> = [...A1, ...A2]
 type Result3 = Push<[1, 2], "3"> // [1, 2, '3']
 
 export type Push<T extends unknown[], U> = [...T, U]
+
+type Result4 = Unshift<[1, 2], "3"> //['3',1,2]
+
+export type Unshift<T extends unknown[], U> = [U, ...T]
