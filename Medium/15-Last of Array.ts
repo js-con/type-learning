8e63a8046ec43268de9fcaed6ef20314c3f5306f
lbw <574524020@qq.com>
2022-03-31 11:41:46 +0800

    fix

--- Medium/15-Last of Array.ts
+++ Medium/15-Last of Array.ts
@@ -4,4 +4,4 @@ type arr2 = [3, 2, 1]
 type tail1 = Last<arr1> // expected to be 'c'
 type tail2 = Last<arr2> // expected to be 1
 
-export type Last<T> = T extends [...infer A, infer B] ? B : never
+export type Last<T> = T extends [...infer _, infer B] ? B : never
