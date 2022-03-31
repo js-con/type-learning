295ece28adac268e9feace16875495af4321e3a2
lbw <574524020@qq.com>
2022-03-31 11:41:20 +0800

    Push

--- Easy/533-Concat&3057-Push.ts
+++ Easy/533-Concat&3057-Push.ts
@@ -2,3 +2,7 @@ type Result = Concat<[1], [2]> // expected to be [1, 2]
 type Result2 = Concat<[], [2]> // expected to be [2]
 
 export type Concat<A1 extends unknown[], A2 extends unknown[]> = [...A1, ...A2]
+
+type Result3 = Push<[1, 2], "3"> // [1, 2, '3']
+
+export type Push<T extends unknown[], U> = [...T, U]
